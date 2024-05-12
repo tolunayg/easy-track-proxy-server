4651a27bc30ab609382feaceef76adb0f989225e
tolunayg <tolunaygedikoglu@gmail.com>
2024-05-12 13:53:34 +0300

    Initial commit

---
+++ server.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const request = require('request');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors({
+  allowedHeaders: ['Content-Type', 'X-Requested-With', 'Authorization', 'X-MBX-APIKEY', 'redirect']
+}));
+
+app.get('/sapi/v1/margin/allAssets', (req, res) => {
+  const API_URL = 'https://api.binance.com/sapi/v1/margin/allAssets';
+  const options = {
+    url: API_URL,
+    headers: {
+      'Content-Type': 'application/json',
+      'X-MBX-APIKEY': '9M88haLggrucTacRnx3sKCOc1aw1QvxrsVljlRu0SdR1g8fFyb92icUArZl2uUFt', // Replace 'xxx' with your actual API key
+      'redirect': 'follow'
+    }
+  };
+
+  request(options, (error, response, body) => {
+    if (error) {
+      return res.status(500).json({ type: 'error', message: error.message });
+    }
+    if (response.statusCode !== 200) {
+      return res.status(500).json({ type: 'error', message: 'Failed to fetch data' });
+    }
+
+    res.json(JSON.parse(body));
+  });
+});
+
+// Add a new route for the ticker/price endpoint
+app.get('/api/v3/ticker/price', (req, res) => {
+  const { symbols } = req.query;
+  const API_URL = `https://testnet.binance.vision/api/v3/ticker/price?symbols=${symbols}`;
+
+  request(API_URL, (error, response, body) => {
+    if (error) {
+      return res.status(500).json({ type: 'error', message: error.message });
+    }
+    if (response.statusCode !== 200) {
+      return res.status(500).json({ type: 'error', message: 'Failed to fetch data' });
+    }
+
+    res.json(JSON.parse(body));
+  });
+});
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));

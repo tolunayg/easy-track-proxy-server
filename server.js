895dbafb8752624708368aa2f8711c07cf8cf41b
tolunayg <tolunaygedikoglu@gmail.com>
2024-05-19 16:55:56 +0300

    Add new endpoints

--- server.js
+++ server.js
@@ -34,7 +34,17 @@ app.get('/sapi/v1/margin/allAssets', (req, res) => {
 // Add a new route for the ticker/price endpoint
 app.get('/api/v3/ticker/price', (req, res) => {
   const { symbols } = req.query;
-  const API_URL = `https://testnet.binance.vision/api/v3/ticker/price?symbols=${symbols}`;
+
+  var API_URL;
+  // Check if symbols parameter is present
+  if (symbols) {
+    // If symbols parameter is present, use it directly in the API URL
+    API_URL = `https://testnet.binance.vision/api/v3/ticker/price?symbols=${symbols}`;
+  } else {
+    // If symbols parameter is not present, make the API call to the base endpoint
+    API_URL = 'https://testnet.binance.vision/api/v3/ticker/price';
+  }
+
 
   request(API_URL, (error, response, body) => {
     if (error) {
@@ -47,6 +57,27 @@ app.get('/api/v3/ticker/price', (req, res) => {
     res.json(JSON.parse(body));
   });
 });
+
+// Add a new route for the klines endpoint (candlestick data)
+app.get('/api/v3/klines', (req, res) => {
+  const { symbol, interval } = req.query;
+  const API_URL = `https://testnet.binance.vision/api/v3/klines?symbol=${symbol}&interval=${interval}`;
+
+  request(API_URL, (error, response, body) => {
+    if (error) {
+      return res.status(500).json({ type: 'error', message: error.message });
+    }
+    if (response.statusCode !== 200) {
+      return res.status(500).json({ type: 'error', message: 'Failed to fetch data' });
+    }
+
+    res.json(JSON.parse(body));
+  });
+});
+
+
+
+
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
